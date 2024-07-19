56080f4ddfd1f04a1ea79942305c06b7dd5f956e
chenkaiyang <chenkaiyang03@meituan.com>
2024-07-19 17:08:52 +0800

    c8-base

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,9 +1,12 @@
 package com.bignerdranch.android.criminalintent;
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public String getTitle() {
         return mTitle;
@@ -18,9 +21,25 @@ public class Crime {
     }
 
 
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
 
     public Crime() {
         // Generate unique identifier
         mId = UUID.randomUUID();
+        mDate=new Date();
     }
 }
